93533d21a54d9c997df1261ac92ddac7a28b337d
zuhair-credencesoft <zuhair@credencesoft.in>
2022-06-01 01:00:24 -0400

    React Bootstrap Installed.

--- clean-creation/pages/index.js
+++ clean-creation/pages/index.js
@@ -1,7 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Home() {
-  return <>Hello</>;
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>Clean Creation</Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
