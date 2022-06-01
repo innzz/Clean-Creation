960517a9e617ffa5605fc74e100cf0dd40b97e1a
zuhair-credencesoft <zuhair@credencesoft.in>
2022-06-01 06:17:37 -0400

    Plan For Everyone Component Created.

---
+++ clean-creation/components/Home/PlanForEveryone.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styles from "../../styles/component-styles/Plan.module.css";
+
+const PlanForEveryone = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.heading}>
+        <h1>Plans for Everyone</h1>
+        <h2>
+          Consistency in your diet starts with a plan. Choose from 2,3, or 4
+          meals per day for 5,6, or 7 days. <br /> Plans are subscription based
+          but can be changed at any time to meet your needs.
+        </h2>
+      </div>
+      <div className={styles.cards}>
+        <div className={styles.card}>
+          <div className={styles.cardHeading}>
+            <h2>The Lifestyle Plan</h2>
+            <h3>as low as</h3>
+            <h4>$9.76 per meal</h4>
+            <button>CHOOSE</button>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardHeading}>
+            <h2>The Lifestyle Plan</h2>
+            <h3>as low as</h3>
+            <h4>$9.76 per meal</h4>
+            <button>CHOOSE</button>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardHeading}>
+            <h2>The Lifestyle Plan</h2>
+            <h3>as low as</h3>
+            <h4>$9.76 per meal</h4>
+            <button>CHOOSE</button>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardHeading}>
+            <h2>The Lifestyle Plan</h2>
+            <h3>as low as</h3>
+            <h4>$9.76 per meal</h4>
+            <button>CHOOSE</button>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardHeading}>
+            <h2>The Lifestyle Plan</h2>
+            <h3>as low as</h3>
+            <h4>$9.76 per meal</h4>
+            <button>CHOOSE</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlanForEveryone;
--- clean-creation/pages/index.js
+++ clean-creation/pages/index.js
@@ -2,20 +2,13 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Container, Row, Col } from "react-bootstrap";
+import PlanForEveryone from "../components/Home/PlanForEveryone";
 
 const HomeScreen = () => {
   return (
     <>
       <div>
-        <Container>
-          <Row>
-            <Col>
-              <h1 className="">
-                <p className="">Clean Creation</p>
-              </h1>
-            </Col>
-          </Row>
-        </Container>
+        <PlanForEveryone />
       </div>
     </>
   );
