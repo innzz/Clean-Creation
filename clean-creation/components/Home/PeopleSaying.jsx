66f97562b06a3a22f84d6df28e2d1167b10b6e4f
zuhair-credencesoft <zuhair@credencesoft.in>
2022-06-01 08:26:43 -0400

    Carousel Created.

---
+++ clean-creation/components/Home/PeopleSaying.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import styles from "../../styles/component-styles/People.module.css";
+import { Carousel } from "react-bootstrap";
+
+const PeopleSaying = () => {
+  return (
+    <div>
+      <div>
+        <Carousel fade>
+          <Carousel.Item>
+            <div className={styles.imagtext}>
+              <img
+                className={`d-block w-100 ${styles.image}`}
+                src="/carousel.jpg"
+                alt="First slide"
+              />
+            </div>
+
+            <Carousel.Caption>
+              <h3 className={styles.secondHeading}>
+                Food is prepared perfectly. Makes meal prepping more convenient
+                and saves time. Taste great. Highly recommend.
+              </h3>
+              <h4 className={styles.thirdHeading}>~Rhett Majoria</h4>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className={styles.imagtext}>
+              <img
+                className={`d-block w-100 ${styles.image}`}
+                src="/carousel.jpg"
+                alt="First slide"
+              />
+            </div>
+
+            <Carousel.Caption>
+              <h3 className={styles.secondHeading}>
+                I have always been so impressed with everything we've ever had
+                from here. Both quality and taste!!! ~Emily Hoskins I can't say
+                enough good things about this place, friendly and inviting staff
+                that are happy to answer any questions you might have, grab and
+                go meals that will leave you feeling great. Not to mention I
+                spend less money here than at any fast food place. ~Nick Pierce
+                Food is prepared perfectly. Makes meal prepping more convenient
+                and saves time. Taste great. Highly recommend.
+              </h3>
+              <h4 className={styles.thirdHeading}>~Monique Encala</h4>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className={styles.imagtext}>
+              <img
+                className={`d-block w-100 ${styles.image}`}
+                src="/carousel.jpg"
+                alt="First slide"
+              />
+            </div>
+
+            <Carousel.Caption>
+              <h3 className={styles.secondHeading}>
+                I can't say enough good things about this place, friendly and
+                inviting staff that are happy to answer any questions you might
+                have, grab and go meals that will leave you feeling great. Not
+                to mention I spend less money here than at any fast food place.
+                ~Nick Pierce Food is prepared perfectly. Makes meal prepping
+                more convenient and saves time. Taste great. Highly recommend.
+              </h3>
+              <h4 className={styles.thirdHeading}>~Emili Hoskins</h4>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default PeopleSaying;
--- clean-creation/pages/index.js
+++ clean-creation/pages/index.js
@@ -3,19 +3,27 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Container, Row, Col } from "react-bootstrap";
 import PlanForEveryone from "../components/Home/PlanForEveryone";
+<<<<<<< HEAD
 import Navbar from "./screens/navbar";
 import Footer from "./screens/footer";
 import GetStarted from "./screens/Home/getStarted";
 
+=======
+import PeopleSaying from "../components/Home/PeopleSaying";
+>>>>>>> 22370af (Carousel Component Created.)
 
 const HomeScreen = () => {
   return (
     <>
       <div>
         <PlanForEveryone />
+<<<<<<< HEAD
              <Navbar />
              <GetStarted />
              <Footer />
+=======
+        <PeopleSaying />
+>>>>>>> 22370af (Carousel Component Created.)
       </div>
     </>
   );
