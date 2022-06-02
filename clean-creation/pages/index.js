509ef57180faaea92b3d0ae70cad13cd16a29096
zuhair-credencesoft <zuhair@credencesoft.in>
2022-06-02 01:44:01 -0400

    Advertisement Component Created.

--- clean-creation/pages/index.js
+++ clean-creation/pages/index.js
@@ -2,9 +2,9 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import PlanForEveryone from "../components/Home/PlanForEveryone";
 import GetStarted from "./screens/Home/getStarted";
-import HowItWork from '../components/Home/HowItWork';
-
+import HowItWork from "../components/Home/HowItWork";
 import PeopleSaying from "../components/Home/PeopleSaying";
+import AdvertisementOne from "../components/Home/AdvertisementOne";
 
 const HomeScreen = () => {
   return (
@@ -14,6 +14,7 @@ const HomeScreen = () => {
         <PlanForEveryone />
         <HowItWork />
         <PeopleSaying />
+        <AdvertisementOne />
       </div>
     </>
   );
