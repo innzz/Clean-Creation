820505254aa5f1afcca0b55b6a7caf271b9c5e8a
zuhair-credencesoft <zuhair@credencesoft.in>
2022-06-02 03:39:36 -0400

    Advertisement Two Created With Responsive Style.

---
+++ clean-creation/components/Home/AdvertisementTwo.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "../../styles/component-styles/Home-styles/AddTwo.module.css";
+
+const AdvertisementTwo = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.image}>
+          <img src="./one.jpg" alt="" />
+        </div>
+        <div className={styles.card}>
+          <h2>
+            Get Started <br />
+            With a Healthier You
+          </h2>
+          <p>
+            The convenience of a well prepared, healthy, flavorful meal with no
+            shopping, preparing, or clean up is a total game changer for your
+            lifestyle!
+          </p>
+          <button>SIGN UP NOW</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdvertisementTwo;
--- clean-creation/pages/index.js
+++ clean-creation/pages/index.js
@@ -5,6 +5,7 @@ import GetStarted from "./screens/Home/getStarted";
 import HowItWork from "../components/Home/HowItWork";
 import PeopleSaying from "../components/Home/PeopleSaying";
 import AdvertisementOne from "../components/Home/AdvertisementOne";
+import AdvertisementTwo from "../components/Home/AdvertisementTwo";
 
 const HomeScreen = () => {
   return (
@@ -15,6 +16,7 @@ const HomeScreen = () => {
         <HowItWork />
         <PeopleSaying />
         <AdvertisementOne />
+        <AdvertisementTwo />
       </div>
     </>
   );
