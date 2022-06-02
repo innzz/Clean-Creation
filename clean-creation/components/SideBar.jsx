7ed616418d3babc17baaa3e899acccd66ed8b88b
Rekha Baitharu <72329941+rekha-baitharu@users.noreply.github.com>
2022-06-02 17:51:04 +0530

    Complited Sidebar

--- clean-creation/components/SideBar.jsx
+++ clean-creation/components/SideBar.jsx
@@ -1,61 +1,93 @@
 import React from "react";
 export default class SideBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hover: false
+        }
+    }
     render() {
         return (
             <div>
-                <aside className="w-64" aria-label="Sidebar">
+                <aside className="w-80" aria-label="Sidebar">
                     <div className="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
-                        <div className="mb-20">
-                            <img src="https://cleancreations.com/data/config/image_placeholder.jpg?md=5aa06eb7aa5a83d1992520b885d63182" class="max-w-full h-auto rounded-full" alt="" />
+                        <div className="mb-20 rounded-full">
+                            <img src="https://cleancreations.com/data/config/image_placeholder.jpg?md=5aa06eb7aa5a83d1992520b885d63182" class=" bg-cover max-w-full h-auto rounded-full" alt="" />
                         </div>
                         <p className="pl-2 text-lg font-semibold text-gray-600">Welcome</p>
-                        <p className="-mt-4 pl-6 text-xs text-gray-500">Credencesoft</p>
+                        <p className="-mt-4 pl-6 text-xs text-gray-500" onMouseEnter={() => { this.setState({ hover: true }) }}
+                            onMouseLeave={() => { this.setState({ hover: false }) }} style={{ color: this.state.hover === true ? 'green' : 'black' }}>Credencesoft</p>
                         <ul className="space-y-2 mt-10">
                             <li className="mb-3">
-                                <a href="#" className="no-underline flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                    <img className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" src="/logout.svg" />
+                                <a href="#" onMouseEnter={() => { this.setState({ hover1: true }) }}
+                                    onMouseLeave={() => { this.setState({ hover1: false }) }} style={{ backgroundColor: this.state.hover1 === true ? 'green' : '', color:this.state.hover1 === true ? "white" :"black" }} className="no-underline flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                                    <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"> <g> <path fill="none" d="M0 0h24v24H0z" /> <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2a9.985 9.985 0 0 1 8 4h-2.71a8 8 0 1 0 .001 12h2.71A9.985 9.985 0 0 1 12 22zm7-6v-3h-8v-2h8V8l5 4-5 4z" /> </g> </svg>
                                     <span className="ml-3">Log off</span>
                                 </a>
                             </li>
                             <hr />
                             <li>
-                                <a href="#" className="no-underline flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                    <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
-                                    <span className="flex-1 ml-3 whitespace-nowrap">Kanban</span>
-                                    <span className="inline-flex justify-center items-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
+                                <a onMouseEnter={() => { this.setState({ hover2: true }) }}
+                                    onMouseLeave={() => { this.setState({ hover2: false }) }}  style={{ backgroundColor: this.state.hover2 === true ? 'green' : '', color:this.state.hover2 === true ? "white" :"black" }} href="#" className="no-underline flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                                    <span className="flex-1 ml-3 whitespace-nowrap">My Profile</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="#" className="no-underline flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                    <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
-                                    <span className="flex-1 ml-3 whitespace-nowrap">Inbox</span>
-                                    <span className="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full dark:bg-blue-900 dark:text-blue-200">3</span>
-                                </a>
+                                <button type="button"  style={{ backgroundColor: this.state.hover3 === true ? 'green' : '', color:this.state.hover3 === true ? "white" :"black" }} class="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                                    <span onMouseEnter={() => { this.setState({ hover3: true }) }}
+                                        onMouseLeave={() => { this.setState({ hover3: false }) }}  class="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>My Subscription</span>
+                                </button>
+                                <ul id="dropdown-example" class=" py-2 space-y-2">
+                                    <li>
+                                        <a onMouseEnter={() => { this.setState({ hover4: true }) }}
+                                            onMouseLeave={() => { this.setState({ hover4: false }) }}  style={{ backgroundColor: this.state.hover4 === true ? 'green' : '', color:this.state.hover4 === true ? "white" :"black" }} href="#" class="no-underline flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" /> </svg>
+                                            Subscription</a>
+                                    </li>
+                                    <li>
+                                        <a onMouseEnter={() => { this.setState({ hover5: true }) }}
+                                            onMouseLeave={() => { this.setState({ hover5: false }) }}  style={{ backgroundColor: this.state.hover5 === true ? 'green' : '', color:this.state.hover5 === true ? "white" :"black" }} href="#" class="no-underline flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" /> </svg>
+                                            Select meals</a>
+                                    </li>
+                                    <li>
+                                        <a onMouseEnter={() => { this.setState({ hover6: true }) }}
+                                            onMouseLeave={() => { this.setState({ hover6: false }) }}  style={{ backgroundColor: this.state.hover6 === true ? 'green' : '', color:this.state.hover6 === true ? "white" :"black" }} href="#" class="no-underline flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" /> </svg>
+                                            Macro Tracking</a>
+                                    </li>
+                                </ul>
                             </li>
+
                             <li>
-                                <a href="#" className="no-underline flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                    <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-                                    <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
-                                </a>
+                                <button  style={{ backgroundColor: this.state.hover7 === true ? 'green' : '', color:this.state.hover7 === true ? "white" :"black" }} type="button" class="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                                    <span onMouseEnter={() => { this.setState({ hover7: true }) }}
+                                        onMouseLeave={() => { this.setState({ hover7: false }) }}  class="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>My Orders</span>
+                                </button>
+                                <ul id="dropdown-example" class=" py-2 space-y-2">
+                                    <li>
+                                        <a onMouseEnter={() => { this.setState({ hover8: true }) }}
+                                            onMouseLeave={() => { this.setState({ hover8: false }) }}  style={{ backgroundColor: this.state.hover8 === true ? 'green' : '', color:this.state.hover8 === true ? "white" :"black" }} href="#" class="no-underline flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" /> </svg>
+                                            Bags
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a onMouseEnter={() => { this.setState({ hover9: true }) }}
+                                            onMouseLeave={() => { this.setState({ hover9: false }) }}  style={{ backgroundColor: this.state.hover9 === true ? 'green' : '', color:this.state.hover9 === true ? "white" :"black" }} href="#" class="no-underline flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" /> </svg>
+                                            Checkouts
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a onMouseEnter={() => { this.setState({ hover10: true }) }}
+                                            onMouseLeave={() => { this.setState({ hover10: false }) }}  style={{ backgroundColor: this.state.hover10 === true ? 'green' : '', color:this.state.hover10 === true ? "white" :"black" }}  href="#" class="no-underline flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" /> </svg>
+                                            Digital Wallet Transactions
+                                        </a>
+                                    </li>
+                                </ul>
                             </li>
-                            <li>
-                                <a href="#" className="no-underline flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                    <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
-                                    <span className="flex-1 ml-3 whitespace-nowrap">Products</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" className="no-underline flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                    <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"></path></svg>
-                                    <span className="flex-1 ml-3 whitespace-nowrap">Sign In</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" className="no-underline flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                                    <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path></svg>
-                                    <span className="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
-                                </a>
-                            </li>
+
                         </ul>
                     </div>
                 </aside>
--- clean-creation/pages/meals.jsx
+++ clean-creation/pages/meals.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
+import ContinueToCart from "../components/Meals/ContinueToCart";
+import SideBar from "../components/SideBar";
 
 const Meal = () => {
-  return <>Meals</>;
+  return <div class="flex gap-4 ">
+    <div>
+      <SideBar />
+    </div>
+    <div className="">
+      <ContinueToCart />
+    </div>
+  </div>;
 };
 
 export default Meal;
