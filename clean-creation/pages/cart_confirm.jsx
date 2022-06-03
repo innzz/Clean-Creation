584aba01020b66db741de6d4a1a343b510fc4b40
Rekha Baitharu <72329941+rekha-baitharu@users.noreply.github.com>
2022-06-03 16:20:04 +0530

    Complited User Details Page (Subscription Meal Selection -Left Section)

--- clean-creation/pages/cart_confirm.jsx
+++ clean-creation/pages/cart_confirm.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import {
+  Col,
+  Row,
+} from "react-bootstrap";
+import CartConfirmation from "../components/Meals/CartConfirmation";
 
 const CartScreen = () => {
-  return <div>CartScreen</div>;
+  return <div className="container">
+    <Row>
+      <Col>
+        <CartConfirmation />
+      </Col>
+      <Col></Col>
+    </Row>
+  </div>;
 };
 
 export default CartScreen;
