06182a873536ef938934f25697b14775c10923df
zuhair-credencesoft <zuhair@credencesoft.in>
2022-06-03 05:33:44 -0400

    Text Color Changed Of Cart Screen.

--- clean-creation/pages/meals.jsx
+++ clean-creation/pages/meals.jsx
@@ -5,16 +5,17 @@ import SideBar from "../components/SideBar";
 import RandomMeal from "../components/Meals/RandomMeal";
 
 const Meal = () => {
-  return <div class="flex gap-4 ">
-    <div className="w-60">
-      <SideBar />
+  return (
+    <div class="flex gap-4 ">
+      <div className="w-60">
+        <SideBar />
+      </div>
+      <div>
+        <RandomMeal />
+        <ContinueToCart />
+      </div>
     </div>
-    <div>
-      <RandomMeal />
-      <ContinueToCart />
-    </div>
-
-  </div>;
-}
+  );
+};
 
 export default Meal;
