425babb9a575ffc49dd3dc1339d73c74aa977da6
Rekha Baitharu <72329941+rekha-baitharu@users.noreply.github.com>
2022-06-04 12:35:40 +0530

    Completed Extras Page

--- clean-creation/pages/shops/customMealBuilder.js
+++ clean-creation/pages/shops/customMealBuilder.js
@@ -1,6 +1,6 @@
 import React from "react";
-import CustomMealDate from "../components/Meals/CustomMealDate";
-import CustomMealPack from "../components/Meals/CustomMealPack";
+import CustomMealDate from "../../components/Shop/CustomMealDate";
+import CustomMealPack from "../../components/Shop/CustomMealPack";
 export default class CustomMeal extends React.Component{
     render(){
         return(
---
+++ clean-creation/pages/shops/extras.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Extras from "../../components/Shop/Extras";
+export default class Extrass extends React.Component{
+    render(){
+        return(
+            <div>
+                <Extras />
+            </div>
+        )
+    }
+}
