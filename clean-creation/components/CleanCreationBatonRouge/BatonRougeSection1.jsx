1982f1cbd9aaadeda68301f6ac777d48c6d3c3e7
inzz <inzamampawaskar10@gmail.com>
2022-06-06 20:51:02 +0530

    Created CleanCreations Baton Rouge Section1 Component

---
+++ clean-creation/components/CleanCreationBatonRouge/BatonRougeSection1.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from '../../styles/component-styles/CleanCreationBatonRouge/BatonRougeSection1.module.css';
+
+function BatonRougeSection1() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+      <h1>CLEAN CREATIONS BATON ROUGE</h1>
+      <div className={styles.description}>
+      <p>Living in Louisiana means you’re always surrounded by delicious foods, and when you’re busy, it can be easy to fall back on beignets, po’boys, and other hearty meals. But sometimes, you need something light, fresh, and nutritious, and Clean Creations, Baton Rouge is here to help! </p>
+      </div>
+      <div className={styles.button}>
+          VIEW MENU
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default BatonRougeSection1
---
+++ clean-creation/pages/clean_creations_baton_rouge.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BatonRougeSection1 from '../components/CleanCreationBatonRouge/BatonRougeSection1'
+
+function clean_creations_baton_rouge() {
+  return (
+    <div>
+      <BatonRougeSection1 />
+    </div>
+  )
+}
+
+export default clean_creations_baton_rouge
