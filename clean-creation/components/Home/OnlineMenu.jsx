5480e5983f2e7a9f3e8dc73a10d6f4bab0a82334
inzz <inzamampawaskar10@gmail.com>
2022-06-08 19:23:08 +0530

    Made OnlineMenu component Dynamic and also working on Weekly Subscription Component to make it Dynamic

--- clean-creation/components/Home/OnlineMenu.jsx
+++ clean-creation/components/Home/OnlineMenu.jsx
@@ -1,9 +1,71 @@
 import React from "react";
 import styles from "../../styles/component-styles/Home-styles/OnlineMenu.module.css";
-import Image from "next/image";
 import Link from "next/link";
 
 function OnlineMenu() {
+
+  const cardsDetails = [
+    {
+      img: 'https://cleancreations.com/data/meals/251.jpg?md=272ee44ac6e0f0904a5a41adc74bfb66',
+      title: 'Balsamic Chicken Salad (16 oz)',
+      description: 'gluten free, dairy free, nut free',
+      calories: 960
+    },
+    {
+      img: 'https://cleancreations.com/data/meals/254.jpg?md=7abe2d53eb88ee393e14d72956b70c2f',
+      title: "Barb's Boobie Milk Lactation Cookies (1/2 Dozen)",
+      description: "Whole Wheat Oatmeal and Chocolate Chip Lactation Cookies. Highly effective in helping to",
+      calories: 170
+    },
+    {
+      img: 'https://cleancreations.com/data/meals/74.jpg?md=120070094cac59572870acd40546c68f',
+      title: "Breakfast Burrito (Vegan)",
+      description: "JUST EGG, Chorizo spiced tofu, Onion, Jalapeno Topped with Green Onion and Fresh Tomato in",
+      calories: 360
+    },
+    {
+      img: 'https://cleancreations.com/data/meals/50.jpg?md=bb66aa99be815d0998ea8ee5d9ef4918',
+      title: "Breakfast Burrito Bowl with Chipotle Dip (Low Carb)",
+      description: "Whole Eggs, Black Beans, White Cheddar, Tomato over Baby Spinach Served with Chipotle Dip",
+      calories: 380
+    },
+    {
+      img: 'https://cleancreations.com/data/meals/26.jpg?md=c2ba5befd0936023cdd17e4a0b5be930',
+      title: "Breakfast Burrito With Chipotle Dip",
+      description: "Whole Eggs, Black Beans, Turkey Chorizo, Jalapeno, White Cheddar, Tomato on Whole Wheat Tortilla",
+      calories: 430
+    },
+    {
+      img: 'https://cleancreations.com/data/meals/342.jpg?md=62babb1f9ffe074a035e3963685f1af5',
+      title: "Broccoli and Red Pepper Vegan Egg Bites",
+      description: "Served with Fresh Fruit",
+      calories: 350,
+      premium: 'premium'
+    },
+    {
+      img: 'https://cleancreations.com/data/meals/285.jpg?md=847cede9fcdd02dab6872cc5059eaa7f',
+      title: "Carrot Cake Overnight Oats",
+      description: "Overnight oats with almond milk, spices, raisins, Yogurt and pecans",
+      calories: 640,
+    },
+    {
+      img: 'https://cleancreations.com/data/meals/152.jpg?md=afa55d98554a5cec569cbb7f3db6cf25',
+      title: "Cheddar Broccoli Egg Bite",
+      description: "Served with Yogurt Parfait",
+      calories: 350,
+      premium: 'premium'
+    },
+    {
+      img: 'https://cleancreations.com/data/meals/351.jpg?md=9cf68854baff02b930806d916e0b47ee',
+      title: "Chicken Salad over Mixed Greens (Low Carb)",
+      description: "House Chicken Salad, over Mixed Greens served with Honey Mustard and Fresh Fruit",
+      calories: 430,
+      premium: 'premium'
+    },
+  ]
+
+  console.log(cardsDetails)
+
   return (
     <div className={styles.container}>
       <div className={styles.innerContainer}>
@@ -16,177 +78,33 @@ function OnlineMenu() {
         </div>
         <div className={styles.cardsSection}>
           <div className={styles.cards}>
-            <div className={styles.card}>
+            {cardsDetails.map((val,i)=>{
+              return (
+                <div className={styles.card} key={i}>
               <div className={styles.leftImage}>
+                <img src={val.img} alt="" />
+                {val.premium === 'premium'?(<div className={styles.premium}>
+                  premium
+                </div>):''}
               </div>
               <div className={styles.rightContent}>
                 <div className={styles.cardDescription}>
                   <p className={styles.menuTitle}>
-                    Balsamic Chicken Salad (16 oz)
+                    {val.title}
                   </p>
                   <p className={styles.menuDescription}>
-                    gluten free, dairy free, nut free
+                    {val.description}
                   </p>
                 </div>
                 <div className={styles.calories}>
                   <p>
-                    <b>960</b> Calories
+                    <b>{val.calories}</b> Calories
                   </p>
                 </div>
               </div>
             </div>
-            <div className={styles.card}>
-              <div className={styles.leftImage}>
-              </div>
-              <div className={styles.rightContent}>
-                <div className={styles.cardDescription}>
-                  <p className={styles.menuTitle}>
-                    Balsamic Chicken Salad (16 oz)
-                  </p>
-                  <p className={styles.menuDescription}>
-                    gluten free, dairy free, nut free
-                  </p>
-                </div>
-                <div className={styles.calories}>
-                  <p>
-                    <b>960</b> Calories
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className={styles.card}>
-              <div className={styles.leftImage}>
-              </div>
-              <div className={styles.rightContent}>
-                <div className={styles.cardDescription}>
-                  <p className={styles.menuTitle}>
-                    Balsamic Chicken Salad (16 oz)
-                  </p>
-                  <p className={styles.menuDescription}>
-                    gluten free, dairy free, nut free
-                  </p>
-                </div>
-                <div className={styles.calories}>
-                  <p>
-                    <b>960</b> Calories
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className={styles.card}>
-              <div className={styles.leftImage}>
-              </div>
-              <div className={styles.rightContent}>
-                <div className={styles.cardDescription}>
-                  <p className={styles.menuTitle}>
-                    Balsamic Chicken Salad (16 oz)
-                  </p>
-                  <p className={styles.menuDescription}>
-                    gluten free, dairy free, nut free
-                  </p>
-                </div>
-                <div className={styles.calories}>
-                  <p>
-                    <b>960</b> Calories
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className={styles.card}>
-              <div className={styles.leftImage}>
-              </div>
-              <div className={styles.rightContent}>
-                <div className={styles.cardDescription}>
-                  <p className={styles.menuTitle}>
-                    Balsamic Chicken Salad (16 oz)
-                  </p>
-                  <p className={styles.menuDescription}>
-                    gluten free, dairy free, nut free
-                  </p>
-                </div>
-                <div className={styles.calories}>
-                  <p>
-                    <b>960</b> Calories
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className={styles.card}>
-              <div className={styles.leftImage}>
-              </div>
-              <div className={styles.rightContent}>
-                <div className={styles.cardDescription}>
-                  <p className={styles.menuTitle}>
-                    Balsamic Chicken Salad (16 oz)
-                  </p>
-                  <p className={styles.menuDescription}>
-                    gluten free, dairy free, nut free
-                  </p>
-                </div>
-                <div className={styles.calories}>
-                  <p>
-                    <b>960</b> Calories
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className={styles.card}>
-              <div className={styles.leftImage}>
-              </div>
-              <div className={styles.rightContent}>
-                <div className={styles.cardDescription}>
-                  <p className={styles.menuTitle}>
-                    Balsamic Chicken Salad (16 oz)
-                  </p>
-                  <p className={styles.menuDescription}>
-                    gluten free, dairy free, nut free
-                  </p>
-                </div>
-                <div className={styles.calories}>
-                  <p>
-                    <b>960</b> Calories
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className={styles.card}>
-              <div className={styles.leftImage}>
-              </div>
-              <div className={styles.rightContent}>
-                <div className={styles.cardDescription}>
-                  <p className={styles.menuTitle}>
-                    Balsamic Chicken Salad (16 oz)
-                  </p>
-                  <p className={styles.menuDescription}>
-                    gluten free, dairy free, nut free
-                  </p>
-                </div>
-                <div className={styles.calories}>
-                  <p>
-                    <b>960</b> Calories
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className={styles.card}>
-              <div className={styles.leftImage}>
-              </div>
-              <div className={styles.rightContent}>
-                <div className={styles.cardDescription}>
-                  <p className={styles.menuTitle}>
-                    Balsamic Chicken Salad (16 oz)
-                  </p>
-                  <p className={styles.menuDescription}>
-                    gluten free, dairy free, nut free
-                  </p>
-                </div>
-                <div className={styles.calories}>
-                  <p>
-                    <b>960</b> Calories
-                  </p>
-                </div>
-              </div>
-            </div>
+              )
+            })}
           </div>
           <div className={styles.buttonWrapper}>
             <div className={styles.button}>
--- clean-creation/components/Subscription/UserPlanSelection.jsx
+++ clean-creation/components/Subscription/UserPlanSelection.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState} from 'react'
 import styles from '../../styles/component-styles/subscription-styles/UserPlanSelection.module.css';
 import { InputGroup } from 'react-bootstrap';
 import { FormControl } from 'react-bootstrap';
@@ -8,6 +9,57 @@ import { GiMeal } from 'react-icons/gi';
 import { FaShoppingCart } from 'react-icons/fa';
 
 function UserPlanSelection() {
+    const yourPlan = [
+        {btnTxt: 'The Everything Plan',
+        btnDesc: "This plan combines every menu option we offer. It's perfect if you love to mix your meal preferences like low carb, vegan, vegetarian, pescatarian or lifestyle. ***PLEASE NOTE: IN ORDER TO AVOID DUPLICATES OF VARIATIONS YOU MUST SELECT YOUR MEALS WITHIN THE PLAN***"
+        },
+        {btnTxt: 'The Lifestyle Plan',
+        btnDesc: "Every meal in this plan was created to promote a healthy lifestyle. Select from the Normal portion (4-5 oz of lean protein, 2-4 oz of complex carbohydrates and 4-6 oz of nutrient rich vegetables) or Muscle Builder portion (6-8 oz of lean protein, 3-6 oz of complex carbohydrates and 6-8 oz of nutrient rich vegetables)"
+        },
+        {btnTxt: 'The Low Carb Plan',
+        btnDesc: "Our low carb selections are completely grain free and packed with nutrient dense vegetable swaps like zucchini noodles, Cauliflower Corn Grits and Turnip Mash. Menus from this plan include top sellers like Shrimp and Cauliflower Grits, Hickory Burger with Caramelized Onions and Mixed Vegetables and Thai Turkey Lettuce Wrap over Cauliflower Rice."
+        },
+        {btnTxt: 'The Pescatarian Plan',
+        btnDesc: "This plan contains a largely plant-based diet of whole grains, nuts, legumes, produce and healthy fats, with seafood playing a key role as a main protein source. You can expect some delicious healthy takes on Louisiana Classics like Shrimp and Vegan Sausage Jambalaya as well as some International cuisine like our Red Curry Shrimp over Brown Rice with Baby Bok Choy and our Agave Lime Shrimp over Roasted Red Peppers."
+        },
+        {btnTxt: 'The Vegan Plan',
+        btnDesc: "Vegan diets are an effective way to improve health if done correctly! Our completely plant based menus offer a diverse mix of legumes, grains, and vegetables. We keep eating plant based exciting with menu options like BBQ tofu over Cauliflower Corn Grits, Shitake Mushroom Eggroll Bowl and Kale and Quinoa Hummus Bowl."
+        },
+        {btnTxt: 'The Vegaterian Plan',
+        btnDesc: "Apart from the ethical and environmental benefits of cutting meat from your diet, a well-planned vegetarian diet may also reduce your risk of chronic disease, support weight loss and improve the quality of your diet. Our Vegetarian menu items include a diverse mix of fruits, vegetables, grains, healthy fats and proteins. Protein that would typically be provided by meat in your diet, is replaced by a variety of protein-rich plant foods like nuts, seeds, legumes, mushrooms and tofu."
+        },
+    ];
+    const portionSizeBtnTxt = [
+        'Regular',
+        'Muscle Builder (Large)'
+    ];
+    const billingRenewalCycle = [
+        '1 Week',
+        '2 Week',
+        '3 Week'
+    ];
+    const yourPlan2 = [
+        '1 Week Plan with Breakfast',
+        '1 Week Plan without Breakfast'
+    ];
+    const daysPerWeek = [
+        '5 Days Per Week',
+        '6 Days Per Week',
+        '7 Days Per Week'
+    ];
+    const mealsPerDay = [
+        '1 meal',
+        '2 meals',
+        '3 meals',
+        '4 meals'
+    ];
+
+    const [yourPlanBtnDesc, setyourPlanBtnDesc] = useState('');
+
+    const handleYourPlanClick = (val)=>{
+        setyourPlanBtnDesc(val.btnDesc);
+    }
+
   return (
     <div className={styles.outerContainer}>
     <div className={styles.midContainer}>
@@ -25,97 +77,76 @@ function UserPlanSelection() {
               <div className={styles.heading}>
                   <h3>1. Choose your plan</h3>
               </div>
+              <div className={styles.buttonsGroup} >
+                {yourPlan.map((val,index)=>{
+                    return (
+                        <div className={styles.button} key={index} onClick={()=>{handleYourPlanClick(val)}}>
+                            {val.btnTxt}
+                        </div>
+                    )
+                })}
+              </div>
+              <div className={styles.desc}>
+                {yourPlanBtnDesc}
+            </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.heading}>
+                  <h3>2. Choose your Portion Size</h3>
+              </div>
               <div className={styles.buttonsGroup}>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
+                {portionSizeBtnTxt.map((val,i)=>{
+                    return(<div className={styles.button} key={i}>
+                    {val}
+                </div>)})}
               </div>
             </div>
             <div className={styles.card}>
               <div className={styles.heading}>
-                  <h3>1. Choose your plan</h3>
+                  <h3>3. Choose your Billing Renewal Cycle</h3>
               </div>
               <div className={styles.buttonsGroup}>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
+               {billingRenewalCycle.map((val,i)=>{
+                   return( <div className={styles.button} key={i}>
+                    {val}
+                </div>)
+               })}
               </div>
             </div>
             <div className={styles.card}>
               <div className={styles.heading}>
-                  <h3>1. Choose your plan</h3>
+                  <h3>4. Choose your plan</h3>
               </div>
               <div className={styles.buttonsGroup}>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
+                {yourPlan2.map((val,i)=>{
+                    return(<div className={styles.button} key={i}>
+                        {val}
+                    </div>)
+                })}
               </div>
             </div>
             <div className={styles.card}>
               <div className={styles.heading}>
-                  <h3>1. Choose your plan</h3>
+                  <h3>5. How many days per week do you want food?</h3>
               </div>
               <div className={styles.buttonsGroup}>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
-                <div className={styles.button}>
-                    The Everything Plan
-                </div>
+                {daysPerWeek.map((val,i)=>{
+                    return(<div className={styles.button} key={i}>
+                        {val}
+                    </div>)
+                })}
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.heading}>
+                  <h3>6. How many meals per day?</h3>
+              </div>
+              <div className={styles.buttonsGroup}>
+                {mealsPerDay.map((val,i)=>{
+                    return(<div className={styles.button} key={i}>
+                        {val}
+                    </div>)
+                })}
               </div>
             </div>
           </div>
@@ -130,8 +161,18 @@ function UserPlanSelection() {
                   Start Over 
               </div>
             </div>
+            <div className={styles.overViewItem}>
+                <div><li>Plan</li><h3>The Everything Plan</h3></div>
+                <div><li>Portion Size</li><h3>Regular</h3></div>
+                <div><li>Billing Renewal Cycle</li><h3>1 Week</h3></div>
+                <div><li>Billing Plan</li><h3>1 Week Plan with Breakfast</h3></div>
+                <div><li>Days per week</li><h3>5 Days per Week</h3></div>
+                <div><li>Meals per day</li><h3>1 Meal</h3></div>
+                <div><li>Price per meal</li><h3>$13.00</h3></div>
+                <div><li>Starting on</li><h3>2022-06-15</h3></div>
+            </div>
             <div className={styles.bottom}>
-              Starting at
+              Starting at <h4>$65.00</h4>
             </div>
         </div>
       </div>
