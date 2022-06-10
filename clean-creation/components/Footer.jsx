faddb9df1018ac0c6a1ed8d12dbc37910de9cd8f
Rekha Baitharu <72329941+rekha-baitharu@users.noreply.github.com>
2022-06-10 17:55:48 +0530

    Updated Footer

--- clean-creation/components/Footer.jsx
+++ clean-creation/components/Footer.jsx
@@ -422,7 +422,7 @@ export default class FooterHome extends React.Component {
         >
           <Card style={{ width: "450px" }}>
             <DialogTitle>
-              <Typography style={{ color: "green", fontSize: "35px" }}>
+              <Typography style={{ color: "#43b3ae", fontSize: "35px" }}>
                 Newsletter
               </Typography>
               <IconButton
@@ -455,7 +455,7 @@ export default class FooterHome extends React.Component {
               <Button
                 variant="contained"
                 style={{
-                  backgroundColor: "green",
+                  backgroundColor: "#43b3ae",
                   color: "white",
                   width: "100%",
                   marginTop: "10px",
