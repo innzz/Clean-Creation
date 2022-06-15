ea3c6b0be7692cadccfff31db2d76df56ff508ce
inzamam <inzamam@credencesoft.in>
2022-06-15 12:37:43 +0530

    Created API for Data Fetching from Database Using Prisma MySql and Implemented in Frontend.

--- pages/currentmenu.jsx
+++ pages/currentmenu.jsx
@@ -3,16 +3,25 @@ import DateDay from "../components/CurrentMenu/DateDay";
 import FullMenu from "../components/CurrentMenu/FullMenu";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import axios from "axios";
 
-const CurrentMenuScreen = () => {
+const CurrentMenuScreen = ({response}) => {
   return (
     <div>
       <NavBar />
       <DateDay />
-      <FullMenu />
+      <FullMenu response={response}/>
       <Footer />
     </div>
   );
 };
+
+export async function getServerSideProps(context){
+   const data = await fetch("http://localhost:5000/products")
+  const response = await data.json()
+  return {
+    props : {response}
+  }
+}
 
 export default CurrentMenuScreen;
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import next from "next";
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const app = express();
+var cors = require('cors')
+app.use(cors());
 
 //Get product by id
 app.get("/products/:id", async (req: Request, res: Response,next) => {
@@ -35,6 +37,6 @@ app.patch("/products/:id", async (req: Request, res: Response) => {
   return res.status(200).json({ message: "hello world." });
 });
 
-const port = 3000;
+const port = 5000;
 
 app.listen(port, () => console.log(`Server started at port ${port}`));
